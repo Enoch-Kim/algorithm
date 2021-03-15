96b38a2f4a8df3b13bfd3fbe4a19563f1e8ae437
Enoch-Kim <enochkim1220@gmail.com>
2021-03-15 11:55:36 +0900

    Retry kakao intern question

--- kakao/2019intern/1.cpp
+++ kakao/2019intern/1.cpp
@@ -1,6 +1,8 @@
 /*
 https://programmers.co.kr/learn/courses/30/lessons/64061
 */
+
+/* first solve
 #include <string>
 #include <vector>
 
@@ -34,6 +36,52 @@ int solution(vector<vector<int>> board, vector<int> moves) {
             }
             cntMap[col]--;
         }
+    }
+    
+    return answer;
+}
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> board, vector<int> moves) {
+    int answer = 0;
+    
+    int rowSize = board.size();
+    int colSize = board[0].size();
+    
+    vector<int> colMap(colSize, rowSize);
+    vector<int> st(0);
+    
+    for(int col=0; col<colSize; col++){
+        for(int row=0; row<rowSize; row++){
+            if(board[row][col]){
+                colMap[col] = row;
+                break;
+            }
+        }
+    }
+    
+    for(int i=0; i<moves.size(); i++){
+        int col = moves[i]-1;
+        int row = colMap[col];
+        if(row == rowSize){
+            continue;
+        }
+        
+        int temp = board[row][col];
+        colMap[col]++;
+        
+        if(!st.empty() && st.back() == temp){
+            st.pop_back();
+            answer += 2;
+            continue;
+        }
+        
+        st.push_back(temp);
     }
     
     return answer;
